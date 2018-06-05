ff768f474ae1f84ea4ab7843b33055c8db0249fe
Sabrina Kennedy <skenne21@gmail.com>
2018-06-05 16:17:06 -0600

    Fix migrations file

--- db/migrations/20180529142202_initial.js
+++ db/migrations/20180529142202_initial.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('denver', 
@@ -19,25 +17,4 @@ exports.down = function(knex, Promise) {
     knex.schema.dropTable('denver')
   ])
 };
-=======
 
-exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('denver', 
-      function(table) {
-        table.increments('id').primary();
-        table.string('Latitude');
-        table.string('Longitude');
-        table.string('Day');
-        table.string('Time');
-        table.integer('DNI').unsigned();
-      })
-  ])
-};
-
-exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('denver')
-  ])
-};
->>>>>>> lint script file for tests
